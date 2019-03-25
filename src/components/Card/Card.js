7bf7b75a3523b334891863c0e33693a884d0caf0
angeljsd <angeljose9629@gmail.com>
2019-03-25 14:10:19 -0500

    Add new crib done. All properties of a crib are showed on Crib componnent

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,59 @@ function Card(props){
         <div className="col-sm-4 mt-3">
             <div className="card">
                 <img className="card-img-top" src={"./assets/images/"+props.crib.image+".jpg"} alt="Card image cap"></img>
-                <div className="card-body">{props.crib.address}</div>
+                <div className="card-body">
+                <div>
+                    <h4>
+                    <span className="badge badge-primary">
+                        { props.crib.type }
+                    </span>
+                    </h4>
+                    <h3>
+                    <i className="fa fa-tag"></i> 
+                        { props.crib.price}
+                    </h3>
+                    <h4>
+                    <i className="fa fa-home"></i> 
+                        { props.crib.address }
+                    </h4>
+
+                    <hr/>
+
+                    <button 
+                        className="btn btn-sm btn-success"
+                    >
+                    Details
+                    </button>
+
+                </div>
+
+                <div>
+                    <div className="details">
+                    <h4>
+                        <span className="badge badge-primary mr-1">
+                        Beds: { props.crib.bedrooms }
+                        </span>
+                        <span className="badge badge-primary mr-1">
+                        Baths: { props.crib.bathrooms }
+                        </span>
+                        <span className="badge badge-primary mr-1">
+                        SqFt: { props.crib.area }
+                        </span>
+                    </h4>
+
+                    <p>{ props.crib.description }</p>
+
+                    <hr/>
+
+                    <button
+                        className="btn btn-sm btn-danger"
+                    >
+                        Close
+                    </button>
+
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
         
--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -8,9 +8,12 @@ class Listing extends React.Component{
     constructor(){
         super()
         this.state = {
-            cribs: []
+            cribs: [],
+            sortDirection: 'asc',
+            sortField: 'price'
         }
         this.newCribCallback = this.newCribCallback.bind(this)
+        this.handleOnChange = this.handleOnChange.bind(this)
     }
 
     componentDidMount(){
@@ -21,6 +24,7 @@ class Listing extends React.Component{
             })
             .then(data => {
                 this.setState({cribs: data})
+                this.sortCribs()
                 //console.log(this.state.cribs)
             })
     }
@@ -31,22 +35,126 @@ class Listing extends React.Component{
         this.setState(prevState => {
             const newCribs = [data, ...prevState.cribs]
             return{
-                cribs: newCribs
+                cribs: newCribs,
+                sortDirection: 'none'
             }
         })
     }
 
+    handleOnChange(event){
+        
+        if(event.target.name==="sort-field"){
+            this.setState({sortField: event.target.value})
+        }
+        else if(event.target.name==="asc"){
+            this.setState(prevState => {
+                return{
+                    sortDirection: 'asc'
+                }
+            })
+        }
+        else if(event.target.name==="des"){
+            this.setState(prevState => {
+                return{
+                    sortDirection: 'des'
+                }
+            })
+        }
+        
+        this.sortCribs()
+    }
+
+    sortCribs(){
+        
+        this.setState(prevState =>{
+            console.log(prevState.sortField)
+            let modifier = 1;
+
+            if (prevState.sortDirection === "des") {
+                modifier = -1;
+            }
+
+            const sortedCribs = prevState.cribs.sort((a, b) => {
+
+                if (a[prevState.sortField] < b[prevState.sortField]) {
+                    return -1 * modifier;
+                } else {
+                    return 1 * modifier;
+                }
+            });
+            
+            return{
+                cribs: sortedCribs
+            }
+        }) 
+    }
+
     render(){
+        
+        let sortFields = [
+            'price',
+            'address',
+            'area',
+            'bathrooms',
+            'bedrooms',
+            'type'
+        ];
+        let sortOptions = sortFields.map(field =>{
+            return  <option value={field}> {field} </option>
+        })
 
         const cardCribs = this.state.cribs.map( crib =>{
             return <Card key={crib.id} crib={crib} />
         })
 
         return(
-            
+
             <div className="container">
                 <AddListing newCribCallback={this.newCribCallback}/>
-            
+
+                
+                    <div className="card mt-3">
+                    <div className="card-body">
+                        <div className="row">
+                        <div className="col-sm-4">
+                        <label>Sort By</label>
+                        <select 
+                            className="form-control" 
+                            name="sort-field" 
+                            id="sort-field"
+                            onChange={this.handleOnChange} 
+                            >
+                            {sortOptions}
+                        </select>
+                        
+                        </div>
+                        <div className="col-sm-4">
+                        <label>Sort Direction</label>
+                        <div>
+                            
+                            <button 
+                                className={"btn btn-primary mr-1 "+(this.state.sortDirection==='asc' && "btn-success")}
+                                name="asc"
+                                onClick={this.handleOnChange}
+                            >
+                                Asc
+                            </button>
+
+                            <button 
+                                className={"btn btn-primary mr-1 "+(this.state.sortDirection==='des' && "btn-success")}
+                                name="des"
+                                onClick={this.handleOnChange}
+                            >
+                                Des
+                            </button>
+
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                
+
                 <div className="row">
                     
                     {cardCribs}
